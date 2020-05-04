531617d3a4f8b8be8d033a8ce7c81673c97689a0
Nguyen Khac Gia Bao <Giabaoproo347@gmail.com>
2020-05-04 15:53:35 +0700

    add http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {ButtonDirective} from './angular-ui-tree/button';
 import {TreeModule} from 'ng2-tree';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonDirective
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        TreeModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TreeModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
